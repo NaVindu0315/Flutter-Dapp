96ecc6333c808430fd8420edb814c1734862bf8a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-05 12:54:54 +0530

    copied another code, shit didnt work

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Voting(),
+      home: MainScreen(),
     );
   }
 }
