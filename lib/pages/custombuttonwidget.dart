8dc7fdf27093e8f7be6d0254146f38453fc62a2e
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-05 20:22:54 +0530

    widget for new test app

--- lib/pages/custombuttonwidget.dart
+++ lib/pages/custombuttonwidget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-Widget customButtonWidget ()
-{
+Widget customButtonWidget() {
   return Container(
-
-  )
+    height: 50,
+    width: 150,
+    color: Colors.black,
+  );
 }
