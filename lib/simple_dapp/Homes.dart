b46bbde00f4c84cb5024bb5b9546e6bb1c45231d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-10 13:40:22 +0530

    Homescreen

---
+++ lib/simple_dapp/Homes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homes extends StatefulWidget {
+  const Homes({Key? key}) : super(key: key);
+
+  @override
+  State<Homes> createState() => _HomesState();
+}
+
+class _HomesState extends State<Homes> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DAPP'),
+        ),
+      ),
+    );
+  }
+}
