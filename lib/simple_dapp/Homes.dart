ab81384064b4bdcf5bc789b86db4a9f0a7b34159
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-10 18:44:20 +0530

    added slider

--- lib/simple_dapp/Homes.dart
+++ lib/simple_dapp/Homes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class Homes extends StatefulWidget {
   const Homes({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class Homes extends StatefulWidget {
 }
 
 class _HomesState extends State<Homes> {
+  double _value = 0.0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,40 +17,61 @@ class _HomesState extends State<Homes> {
         appBar: AppBar(
           title: Text('DAPP'),
         ),
-        body: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.13,
-              decoration: BoxDecoration(
-                color: Colors.deepPurple..withOpacity(.4),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Current Blaance',
-                        style: TextStyle(
-                          fontSize: 30.0,
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.13,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple..withOpacity(.4),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Current Blaance',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 12.0,
-                      ),
-                      Text(
-                        '0',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      )
-                    ],
+                        SizedBox(
+                          height: 12.0,
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20.0,
+              ),
+              SfSlider(
+                value: _value,
+                onChanged: (value) {
+                  setState(() {
+                    _value = value;
+                  });
+                },
+                interval: 1,
+                activeColor: Colors.white,
+                enableTooltip: true,
+                stepSize: 1.0,
+                showLabels: true,
+                min: 0.0,
+                max: 10.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
